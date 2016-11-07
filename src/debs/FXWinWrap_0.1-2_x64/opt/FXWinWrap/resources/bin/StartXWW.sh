58536a1b8dfaf248f55b4e586709b3308f58f292
Maxim <gamer1119@gmail.com>
2016-11-06 19:25:24 -0600

    Update StartXWW.sh
    Removed nohup to keep from flushing to disk.

--- src/debs/FXWinWrap_0.1-2_x64/opt/FXWinWrap/resources/bin/StartXWW.sh
+++ src/debs/FXWinWrap_0.1-2_x64/opt/FXWinWrap/resources/bin/StartXWW.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 ### nohup is needed to keep child process alive from java's
-xterm -e nohup ~/.animatedBGstarter.sh
-xterm -e nohup ~/.animatedBGstarter2.sh
+xterm -e ~/.animatedBGstarter.sh
+xterm -e ~/.animatedBGstarter2.sh
